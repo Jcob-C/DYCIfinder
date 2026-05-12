bd93dde07b286649d61648c037790b520dca1a9d
JcobC <arjhaycelestino54@gmail.com>
2026-05-12 16:30:00 +0800

    update for set manual status

--- server/api/admin/set_foundreport_status.php
+++ server/api/admin/set_foundreport_status.php
@@ -4,6 +4,8 @@ session_start();
 
 require_once __DIR__ . '/../../conf/db.php';
 require_once __DIR__ . '/../../db/found_reports.php';
+require_once __DIR__ . '/../../db/foundreport_claims.php';
+require_once __DIR__ . '/../../db/lost_reports.php';
 require_once __DIR__ . '/../../lib/util.php';
 
 admin_block();
@@ -11,6 +13,14 @@ admin_block();
 try {
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     $inputs = json_decode(file_get_contents("php://input"), true);
+
+    if ($inputs['status'] == "Pending" || $inputs['status'] == "Unclaimed") {
+        set_claims_statuses($conn, $inputs['id'], "Pending");
+        reset_lost_status($conn, $inputs['id']);
+    }
+    if ($inputs['status'] == "To-claim" || $inputs['status'] == "Claimed") {
+        set_claims_statuses($conn, $inputs['id'], "Rejected");
+    }
 
     $success = set_report_status($conn, $inputs['id'], $inputs['status']);
     if ($success) {
